7779cede9b38c8052b1f82d0f370ab1200bfd5ec
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-03 08:23:09 -0600

    feat: 🎸 add header

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className='sticky top-0 z-50 w-full bg-background'>
+      <div className='container flex h-24 items-center'>
+        <h1 className='text-xl md:text-2xl font-bold text-primary'>
+          Traxi Frontend Challeng
+        </h1>
+      </div>
+    </header>
+  )
+}
