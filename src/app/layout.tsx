ffff9b938d35b8db4be875533474be69baf4dec4
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-03 16:39:52 -0600

    feat: 🎸 add styles with cn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
 import Footer from '@/components/footer'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -34,13 +35,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='es' suppressHydrationWarning>
-      <body className={inter.className}>
+      <body
+        className={cn(
+          'min-h-screen bg-background antialiased flex flex-col',
+          inter.className,
+        )}>
         <ThemeProvider
           enableSystem={true}
           attribute='class'
           defaultTheme='system'>
           <Header />
-          {children}
+          <div className='flex-1'>{children}</div>
         </ThemeProvider>
         <Footer />
       </body>
