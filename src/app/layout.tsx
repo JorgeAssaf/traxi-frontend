473b3664c8a5f33126145917f057bf7bd819d271
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-03 13:18:17 -0600

    feat: 🎸 add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Header } from '@/components/header'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
+import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -41,6 +42,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Header />
           {children}
         </ThemeProvider>
+        <Footer />
       </body>
     </html>
   )
