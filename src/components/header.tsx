443eb299c2f436eea923685c408ab47397598e60
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-03 18:12:35 -0600

    fix: 🐛 challenge

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ export const Header = () => {
   return (
     <header className='sticky top-0 z-50 w-full bg-background'>
       <div className='container flex justify-between h-24 items-center'>
-        <h1 className='text-xl md:text-2xl font-bold text-primary'>
-          Traxi Frontend Challeng
+        <h1 className='text-lg md:text-2xl font-bold text-primary'>
+          Traxi Frontend Challenge
         </h1>
         <ThemeToggle />
       </div>
