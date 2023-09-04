46eef24930cf0e58f588bcd97a35b287ceeac417
Jorge Assaf <jorgeassaf160@gmail.com>
2023-09-03 18:12:07 -0600

    feat: 🎸 add filters and types

--- src/components/car-list.tsx
+++ src/components/car-list.tsx
@@ -1,55 +1,64 @@
 'use client'
-import CarMock from '@/assets/carMock.json'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { useState } from 'react'
 import { AlertOctagonIcon } from 'lucide-react'
 import { Button } from './ui/button'
 import { Label } from './ui/label'
 import { Input } from './ui/input'
+import type { CarMockWithCoords } from '@/types/coords'
 
 interface CarListProps {
-  CarMock: typeof CarMock
-  selectedCar: React.Dispatch<React.SetStateAction<typeof CarMock[0] | null>>
+  CarMock: CarMockWithCoords[]
+  selectedCar: React.Dispatch<React.SetStateAction<CarMockWithCoords | null>>
 }
 
 const CarList = ({ CarMock, selectedCar }: CarListProps) => {
   const [query, setQuery] = useState('')
   const [currentPage, setCurrentPage] = useState(0)
 
-
   const filteredCars = () => {
+    if (query.length === 0) return CarMock.slice(currentPage, currentPage + 7)
 
-    if (query.length === 0)
-      return CarMock.slice(currentPage, currentPage + 6);
+    const filtered = CarMock.filter(
+      (car) =>
+        car.MODEL.toLowerCase().includes(query) ||
+        car.BRAND.toLowerCase().includes(query) ||
+        car.placa.toLowerCase().includes(query),
+    )
 
-    const filtered = CarMock.filter(car => car.MODEL.toLowerCase().includes(query) || car.BRAND.toLowerCase().includes(query))
-
-    return filtered.slice(currentPage, currentPage + 6);
+    return filtered.slice(currentPage, currentPage + 7)
   }
 
-
   const nextPage = () => {
-    if (CarMock.filter(car => car.MODEL.toLowerCase().includes(query)).length > currentPage + 6)
-      setCurrentPage(currentPage + 6);
+    if (
+      CarMock.filter(
+        (car) =>
+          car.MODEL.toLowerCase().includes(query) ||
+          car.BRAND.toLowerCase().includes(query) ||
+          car.placa.toLowerCase().includes(query),
+      ).length >
+      currentPage + 7
+    )
+      setCurrentPage(currentPage + 7)
   }
 
   const prevPage = () => {
-    if (currentPage > 0)
-      setCurrentPage(currentPage - 6);
+    if (currentPage > 0) setCurrentPage(currentPage - 7)
   }
 
-  const handleSelectedCar = (car: any) => {
+  const handleSelectedCar = (car: CarMockWithCoords) => {
     selectedCar({ ...car })
-    console.log(car)
   }
   return (
-    <div>
-      <div className='flex justify-between space-x-8 mt-8'>
+    <>
+      <div className='flex  space-x-8 mt-8'>
         <div className='flex space-x-4 items-center'>
           <Label>Buscar</Label>
-          <Input placeholder='Modelo, Marca o Placa' onChange={(e) => setQuery(e.target.value)} />
+          <Input
+            placeholder='Modelo, Marca o Placa'
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </div>
-
       </div>
       <div className='mt-8'>
         <div className='flex justify-between space-x-8 mt-8'>
@@ -60,36 +69,37 @@ const CarList = ({ CarMock, selectedCar }: CarListProps) => {
             <ChevronRight />
           </Button>
         </div>
-        <div className='grid grid-cols-1 place-content-center md:grid-cols-2 gap-8 mt-8'>
-          {
-            filteredCars().length > 0 ?
-              filteredCars().map((car) => (
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8 mt-8'>
+          {filteredCars().length > 0 ? (
+            filteredCars().map((car) => (
+              <div
+                className=' w-full border  bg-foretext-foreground shadow-lg  rounded-lg overflow-hidden'
+                key={car['segure numebr']}>
                 <div
-                  className=' w-full border p-2 bg-foretext-foreground shadow-lg  rounded-lg overflow-hidden'
-                  key={car['segure numebr']}
-                >
-                  <div className='px-4 py-2 cursor-pointer' onClick={() => handleSelectedCar(car)}>
-                    <h2 className='text-foreground text-xl font-semibold'>
-                      {car.BRAND} {car.MODEL}
-                    </h2>
-                    <p className='text-muted-foreground text-sm'>
-                      {car.YEAR} - {car.COLOR}
-                    </p>
-                  </div>
+                  className='px-4 py-2'
+                  onClick={() => handleSelectedCar(car)}>
+                  <h2 className='text-foreground text-xl font-semibold'>
+                    {car.BRAND} {car.MODEL}
+                  </h2>
+                  <p className='text-muted-foreground text-sm my-2'>
+                    {car.YEAR} - {car.COLOR}
+                  </p>
+                  <p>
+                    Placa: <span className='text-foreground'>{car.placa}</span>
+                  </p>
                 </div>
-              ))
-              :
-              <div className='flex flex-col items-center space-y-4'>
-                <AlertOctagonIcon size={64} />
-                <p className='text-foreground'>No se encontraron resultados</p>
 
               </div>
-          }
+            ))
+          ) : (
+            <div className='flex flex-col items-center space-y-4'>
+              <AlertOctagonIcon size={74} />
+              <p className='text-foreground'>No se encontraron resultados</p>
+            </div>
+          )}
         </div>
       </div>
-
-    </div >
-
+    </>
   )
 }
 export default CarList
